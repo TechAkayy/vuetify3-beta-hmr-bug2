4c9330b6ef8b90c5911542e0c9caa61d7fc538dc
akkayy <ahmed.kaja@outlook.com>
2022-03-31 13:19:06 +1100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-app>
+		<div v-for="(item, index) in items" :key="index" class="mb-3 ml-1">
+			<v-btn class="justify-center text-subtitle-2 text-red"
+				>{{ item.name }}
+			</v-btn>
+		</div>
+	</v-app>
+</template>
+
+<script>
+	export default {
+		data: () => ({
+			items: [{ name: 'item1' }, { name: 'item2' }, { name: 'item3' }]
+		})
+	}
+</script>
